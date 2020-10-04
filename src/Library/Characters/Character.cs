375ff2ddbcf89eef0dc397cf2e75b02b49d2d7fd
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-04 18:38:20 -0300

    Adding Character class

---
+++ src/Library/Characters/Character.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+
+namespace RoleplayGame
+{
+    public abstract class Character
+    {
+        private int health;
+
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    this.health = value;
+                }
+                else
+                {
+                    this.health = 0;
+                }
+            }
+        }
+
+        private int attack;
+        public int Attack
+        {
+            get
+            {
+                return this.attack;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    this.attack = value;
+                }
+                else
+                {
+                    this.attack = 0;
+                }
+            }
+        }
+        
+        public string Name;
+
+        private int totalDefense;
+
+        public int TotalDefense
+        {
+            get
+            {
+                return this.totalDefense;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    this.totalDefense = value;
+                }
+                else
+                {
+                    this.totalDefense = 0;
+                }
+            }
+        }
+        
+
+        public Character(string name)
+        {
+            this.Name = name;
+            this.Health = 100;
+        }
+
+        public List<IItem> items;
+        
+
+        public void AttackValue()
+        {
+           int totalAttack = 0;
+
+          foreach (IItem item in items)
+          {
+            totalAttack += item.attackValue;
+          }  
+
+            this.Attack = totalAttack;
+        }
+
+        public void DefenseValue()
+        {
+            int totalDefense = 0;
+            foreach (IItem item in items)
+            {
+                totalDefense += item.defenseValue;
+            }
+
+            this.TotalDefense = totalDefense;
+        }
+
+
+        public void ReceiveAttack(int power)
+        {
+            if (this.TotalDefense < power)
+            {
+                this.Health -= power - this.totalDefense;
+            }
+        }
+
+        public void Cure()
+        {
+            this.health = 100;
+        }
+    }
+
+
+
+}
