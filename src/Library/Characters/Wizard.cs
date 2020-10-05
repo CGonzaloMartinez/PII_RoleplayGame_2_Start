3c4e5781ca0a5da7d9925296c51d5030dbcced7d
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2020-10-04 22:14:36 -0300

    Fixed Dwarf

--- src/Library/Characters/Wizard.cs
+++ src/Library/Characters/Wizard.cs
@@ -3,11 +3,30 @@ namespace RoleplayGame
     public class Wizard: Character
     {
 
+
+        public SpellsBook spellsBook {get;set;}
+        public Staff staff           {get;set;}
+
         public Wizard(string name)
         : base (name) 
         {
         }
 
+        public override int Attack
+        {
+            get
+            {
+                return this.spellsBook.Attack + this.staff.Attack;
+            }
+        }
+
+        public override int Defense
+        {
+            get
+            {
+                return this.spellsBook.Defense + this.staff.Defense;
+            }
+        }
 
     }
 }
