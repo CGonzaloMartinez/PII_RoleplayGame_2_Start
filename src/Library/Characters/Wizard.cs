3867ca739bb580dd9e30328312c3ae3d97f2e613
Mateone <54219667+Mateone@users.noreply.github.com>
2020-10-04 21:21:37 -0300

    arreglé Wizzard

--- src/Library/Characters/Wizard.cs
+++ src/Library/Characters/Wizard.cs
@@ -2,14 +2,10 @@ namespace RoleplayGame
 {
     public class Wizard: Character
     {
-        private int health = 100;
-
         public Wizard(string name)
         : base (name) 
         {
             this.Health = 100;
         }
-
-
     }
 }
